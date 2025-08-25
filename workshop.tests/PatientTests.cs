2a84384cf48abd038d727121de933c83ed2149d9
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-08-25 08:54:29 +0200

    Adds tests

--- workshop.tests/PatientTests.cs
+++ workshop.tests/PatientTests.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using System.Text;
+using workshop.wwwapi.Data;
+using workshop.wwwapi.Repository;
 
 namespace workshop.tests;
 
@@ -14,9 +18,99 @@ public class Tests
         var client = factory.CreateClient();
 
         // Act
-        var response = await client.GetAsync("/patients");
+        var response = await client.GetAsync("/surgery/patients");
 
         // Assert
-        Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
     }
+    [Test]
+    public async Task GetAllDoctors()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/surgery/doctors");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task GetAllAppointments()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/surgery/appointments");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+    }
+
+    [Test]
+    public async Task CreatePatient()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+        var content = new StringContent(JsonConvert.SerializeObject(
+            new { FullName = "Test" }), 
+            Encoding.UTF8, 
+            "application/json"
+            );
+
+        // Act
+        var response = await client.PostAsync("/surgery/patients", content);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+    }
+
+    [Test]
+    public async Task CreateDoctor()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+        var content = new StringContent(JsonConvert.SerializeObject(
+            new { FullName = "Test" }),
+            Encoding.UTF8,
+            "application/json"
+            );
+
+        // Act
+        var response = await client.PostAsync("/surgery/doctors", content);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+    }
+
+    [Test]
+    public async Task CreateAppointments()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+        var content = new StringContent(JsonConvert.SerializeObject(
+            new { PatientId = 1,
+                  PatientName = "Test1",
+                  DoctorId = 1,
+                  DoctorName = "DrTest",
+                  AppointmentDate = DateTime.UtcNow,
+            }),
+            Encoding.UTF8,
+            "application/json"
+            );
+
+        // Act
+        var response = await client.PostAsync("/surgery/appointments", content);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.Created));
+    }
+
 }
